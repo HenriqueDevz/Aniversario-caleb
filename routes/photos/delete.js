c29bfe18fd2f1282ee5f0420267b086725b9d67b
Henrique <henrique_roberto121@hotmail.com>
2026-07-05 09:19:21 -0300

    fix delete log order

--- routes/photos/delete.js
+++ routes/photos/delete.js
@@ -10,8 +10,8 @@ cloudinary.config ({
 })
 
 router.delete('/photos/:id', async (req, res) => {
-    console.log('DELETE /photos chamado, id:', id)
     const { id } = req.params;
+    console.log('DELETE /photos chamado, id:', id)
     const photoResult = await db.execute({
         sql: 'SELECT * FROM photos WHERE id = ?',
         args: [id]
