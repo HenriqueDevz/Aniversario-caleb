5f197d91ad5f3179dc135d32ce0cfe970ca9b29f
Henrique <henrique_roberto121@hotmail.com>
2026-07-05 09:33:52 -0300

    add try catch to delete photo

--- routes/photos/delete.js
+++ routes/photos/delete.js
@@ -3,28 +3,34 @@ const router = express.Router();
 const { db } = require('../../database');
 
 const cloudinary = require('cloudinary').v2;
-cloudinary.config ({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 router.delete('/photos/:id', async (req, res) => {
     const { id } = req.params;
     console.log('DELETE /photos chamado, id:', id)
-    const photoResult = await db.execute({
-        sql: 'SELECT * FROM photos WHERE id = ?',
-        args: [id]
-    })
-    const photo = photoResult.rows[0]
-    if (!photo) return res.status(404).json({ error: 'Photo not found' });
-    await cloudinary.uploader.destroy(photo.public_id)
-
-    await db.execute({
-        sql: 'DELETE FROM photos WHERE id = ?',
-        args: [id]
-    })
-    res.json({ message: 'photo deleted succesfully' });
+    try {
+        const photoResult = await db.execute({
+            sql: 'SELECT * FROM photos WHERE id = ?',
+            args: [id]
+        })
+        const photo = photoResult.rows[0]
+        if (!photo) return res.status(404).json({ error: 'Photo not found' });
+        
+        await cloudinary.uploader.destroy(photo.public_id)
+        
+        await db.execute({
+            sql: 'DELETE FROM photos WHERE id = ?',
+            args: [id]
+        })
+        res.json({ message: 'photo deleted successfully' });
+    } catch (err) {
+        console.error('DELETE error:', err.message || JSON.stringify(err))
+        res.status(500).json({ error: err.message })
+    }
 });
 
 module.exports = router;
